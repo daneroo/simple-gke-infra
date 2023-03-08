93743017a0cc9fdf7df373638deb05a4f00638f8
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-03-08 11:20:43 -0500

    Python missing Z in iso8601 format

--- django-time/django_time/api_app/views.py
+++ django-time/django_time/api_app/views.py
@@ -7,6 +7,6 @@ from django.http import JsonResponse
 
 def current_time(request):
     now = datetime.datetime.now()
-    response_data = {'time': now.strftime('%Y-%m-%d %H:%M:%S'),'lang': 'Django/Python'}
+    response_data = {'time': now.isoformat()+'Z','lang': 'Django/Python'}
     return JsonResponse(response_data)
 
